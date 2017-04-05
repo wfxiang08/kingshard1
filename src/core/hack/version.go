1efb37854edda79881e461d38eb14d55214398fc
wangfei <feiwang@ushow.media>
2017-04-05 11:14:39 +0800

    添加了kingshard初始代码

--- src/core/hack/version.go
+++ src/core/hack/version.go
@@ -1,6 +1,6 @@
 package hack
 
 const (
-	Version = "2017-04-05 08:51:57 +0800 @5d734fb"
-	Compile = "2017-04-05 11:10:03 +0800 by go version go1.8 darwin/amd64"
+	Version = "2017-04-05 11:13:09 +0800 @8d08c84"
+	Compile = "2017-04-05 11:14:34 +0800 by go version go1.8 darwin/amd64"
 )
